509b0bbd50ac874ffbe342c367554116e5424b0b
Bukola Baiyewu <daniellabaiyewu@gmail.com>
2022-04-21 16:17:41 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+
+import Head from 'next/head';
+import { PostWidget, PostCard, Categories } from '../components';
+import { FeaturedPosts } from '../sections';
+import { getPosts } from '../services';
+
+
+const Home= ({posts}) => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <FeaturedPosts />
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+        {
+          posts.map((post, index) => {
+          return <PostCard post={post.node} key={index}/>
+          })
+        }
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+          <PostWidget />
+          <Categories /> 
+          </div>
+
+      </div>
+      </div>
+      
+      
+    </div>
+  )
+}
+
+export default Home
+
+export async function getStaticProps(){
+  const posts = await getPosts()
+  return {
+    props:{posts}
+  }
+}
